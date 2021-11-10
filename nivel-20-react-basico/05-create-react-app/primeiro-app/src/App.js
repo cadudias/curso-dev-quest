3f15e77fd41748f0baf4f1f7bfa16397a1d9ef6e
Roberto Dias <roberto_rhd@hotmail.com>
2021-11-10 10:55:06 -0300

    modulo react

---
+++ nivel-20-react-basico/05-create-react-app/primeiro-app/src/App.js
@@ -0,0 +1,16 @@
+import Botao from './components/button/button'
+
+function App() {
+  return (
+    <>
+      <Botao text='Ac'/>
+      <Botao />
+      <Botao />
+      <Botao />
+      <Botao />
+      <Botao />
+    </>
+  );
+}
+
+export default App;
