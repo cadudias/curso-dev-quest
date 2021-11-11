c4cac0df0454883877925846376c4726f950f584
Roberto Dias <roberto.rhd@gmail.com>
2021-11-11 16:01:09 -0300

    card

--- nivel-20-react-basico/05-create-react-app/primeiro-app/src/App.js
+++ nivel-20-react-basico/05-create-react-app/primeiro-app/src/App.js
@@ -1,11 +1,17 @@
+import Button from './components/button/button';
 import Card from './components/card/card'
 
 function App() {
   return (
     <>
-      <Card title='Card 1'/>
-      <Card title='Card 2'/>
-      <Card title='Card 3'/>
+      <Card>
+        <h3>Titulo card 1</h3>
+        <p>Texto card 1</p>
+      </Card>
+      <Card>
+        <h3>Titulo card 2</h3>
+        <p>Texto card 2</p>
+      </Card>
     </>
   );
 }
--- nivel-20-react-basico/05-create-react-app/primeiro-app/src/components/card/card.js
+++ nivel-20-react-basico/05-create-react-app/primeiro-app/src/components/card/card.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import './card.css'
 
-const Card = ({title}) => {
+const Card = ({children}) => {
     return (
         <div className='card'>
-            <h3>{ title }</h3>
-            <p>Texto card</p>
+            {children}
         </div>
     )
 }
